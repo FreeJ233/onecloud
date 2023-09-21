0184e8275d41ada10a9d40e075fc1cfa4df6668c
FreeJ233 <50964071+FreeJ233@users.noreply.github.com>
2023-09-21 14:45:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,10 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+sed -i "s/192.168.1.1/192.168.5.11/" package/base-files/files/bin/config_generate
+#修改主题名称
+sed -i 's/'OpenWrt'/'OneCloud'/g' package/base-files/files/bin/config_generate
+
+#修改登录密码
+sed -i 's/root::0:0:99999:7:::/root:$1$xx6y.OEi$TtpqSyLy5Fz8SF8f9DRVy.:18217:0:99999:7:::/g' package/base-files/files/etc/shadow
